cd8592b78165584f74cb99b3bcfcb4f4ecd59d4e
msancp06 <msancp05@estudiantes.unileon.es>
2016-04-05 14:32:26 +0200

    Primer commit, poco a poco

---
+++ src/Date.java
@@ -0,0 +1,21 @@
+package es.unileon.prg.date;
+
+/*
+
+
+
+*/
+
+public class Date extends throws DateException {
+
+	private int year;
+	private int month;
+	private int day;
+
+}
+
+public boolean isSameYear() {
+
+	return (this.year == )
+
+}
---
+++ src/MiExcepcion.java
@@ -0,0 +1,15 @@
+package es.unileon.prg.date;
+
+/*
+
+
+
+*/
+
+public class DateException extends Exception {
+
+	public DateException (String errormens){
+
+		super (errormens);
+	}
+}
